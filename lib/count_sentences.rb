52a8599d669ac4546eb5a5bd2952968417a60262
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-28 10:32:49 -0600

    commit tree cdf7681099ace6013f1ff8eab40b0b9cf9c1052c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    splitSentence = self.split(/\.|\?|!/)
+    noEmptySentences = splitSentence.filter{ |word| !word.empty?}
+    noWhiteSpace = noEmptySentences.map(&:strip)
+    noWhiteSpace.length
   end
 end
+
